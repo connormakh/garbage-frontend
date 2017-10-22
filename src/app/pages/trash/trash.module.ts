f891660d4525a16cda43719973317d8da18cfade
Connor <connormakhlouta@gmail.com>
2017-10-22 13:22:24 +0300

    add frontend design for trash cans

--- src/app/pages/trash/trash.module.ts
+++ src/app/pages/trash/trash.module.ts
@@ -6,6 +6,7 @@ import { AngularEchartsModule } from 'ngx-echarts';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { TrashComponent } from './trash.component';
+import {TrashCardComponent} from "./trash-card/trash-card.component";
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { TrashComponent } from './trash.component';
     AngularEchartsModule,
   ],
   declarations: [
-    TrashComponent
+    TrashComponent,
+    TrashCardComponent
   ],
 })
 export class TrashModule { }
